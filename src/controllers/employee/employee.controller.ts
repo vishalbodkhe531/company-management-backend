e212f5a9551552e98970cc159562bc15b4ff6d7b
Vishal Bodkhe <vishalbodkhe@gmail.com>
2025-01-08 14:45:02 +0530

    complited send request API

--- src/controllers/employee/employee.controller.ts
+++ src/controllers/employee/employee.controller.ts
@@ -1,4 +1,62 @@
 import { TryCatch } from "../../middlewares/error.middleware.js";
+import { Employee } from "../../models/emp-model/employee.model.js";
+import errorHandler from "../../utils/errorHandler.utile.js";
+
+export const sendReqeust = TryCatch(async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    phoneNumber,
+    email,
+    resignationDate,
+    qualification,
+    department,
+    address,
+    profilePic,
+    gender,
+    isVerified,
+    role,
+  } = req.body;
+
+  if (
+    !firstName ||
+    !lastName ||
+    !phoneNumber ||
+    !email ||
+    !resignationDate ||
+    !qualification ||
+    !department ||
+    !address ||
+    !gender
+  )
+    return next(new errorHandler("Fill all the fields !!", 400));
+
+  const isExistEmail = await Employee.findOne({ email });
+
+  if (isExistEmail)
+    return next(
+      new errorHandler("Email already exist Wait for accept request!!", 400)
+    );
+
+  !(await Employee.create({
+    firstName,
+    lastName,
+    phoneNumber,
+    email,
+    resignationDate,
+    qualification,
+    department,
+    address,
+    profilePic,
+    gender,
+    isVerified,
+    role,
+  }));
+
+  res
+    .status(200)
+    .json({ success: true, message: "Request Sended successfully !!" });
+});
 
 export const newEmployee = TryCatch(async (req, res, next) => {
   const {
@@ -16,20 +74,9 @@ export const newEmployee = TryCatch(async (req, res, next) => {
     role,
   } = req.body;
 
-  console.log(
-    firstName,
-    lastName,
-    phoneNumber,
-    email,
-    resignationDate,
-    qualification,
-    department,
-    address,
-    profilePic,
-    gender,
-    isVerified,
-    role
-  );
+  const isExistEmp = await Employee.findOne({ email });
+
+  if (isExistEmp) return next(new errorHandler("email already exist !!", 400));
 
   res
     .status(201)
--- src/routes/emp-routes/emp.routes.ts
+++ src/routes/emp-routes/emp.routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { newEmployee } from "../../controllers/employee/employee.controller.js";
+import {
+  newEmployee,
+  sendReqeust,
+} from "../../controllers/employee/employee.controller.js";
 
 const empRoutes = express.Router();
 
 empRoutes.post("/new", newEmployee);
+empRoutes.post("/send-request", sendReqeust);
 
 export default empRoutes;
